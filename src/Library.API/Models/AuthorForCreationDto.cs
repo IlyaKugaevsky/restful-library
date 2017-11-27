195b38f76c566820c7c4c4c0edf67611171b3feb
IlyaNotebook <ilya.kugaevsky@gmail.com>
2017-11-27 06:43:20 -0500

    Add AuthorCollections support, implement custom model binder, fix gitignore to exclude mysql files

--- src/Library.API/Models/AuthorForCreationDto.cs
+++ src/Library.API/Models/AuthorForCreationDto.cs
@@ -10,6 +10,7 @@ namespace Library.API.Models
         public DateTimeOffset DateOfBirth { get; set; }
         public string Genre { get; set; }
 
-        public ICollection<BookForCreationDto> Books {get; set;}
+        public ICollection<BookForCreationDto> Books {get; set;} 
+            = new List<BookForCreationDto>();
     }
 }
